1838878eafe72ec9ab6b9bfd926fc15cde3711c5
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-07-27 11:57:02 +0530

    Time: 2 ms (99.53%), Space: 42.2 MB (86.93%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> map1 = new TreeMap<>();
+        VOT(root , 0 , 0 , map1);
+        List<List<Integer>> list1 = new ArrayList<>();
+
+        for(TreeMap<Integer , PriorityQueue<Integer>> yAxis : map1.values()){
+            List<Integer> list2 = new ArrayList<>();
+            for(PriorityQueue<Integer> pq : yAxis.values()){
+                while(!pq.isEmpty()){
+                    list2.add(pq.poll());
+                }
+            }
+            list1.add(list2);
+        }
+
+        return list1;
+        
+
+
+        
+
+
+
+        
+    }
+
+    public static void VOT(TreeNode root , int x , int y , TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> map1){
+
+        if(root == null){
+            return;
+        }
+
+        if(!map1.containsKey(x)){
+            map1.put(x , new TreeMap<>());
+        }
+        if(!map1.get(x).containsKey(y)){
+            map1.get(x).put(y , new PriorityQueue<>());
+        }
+
+        map1.get(x).get(y).offer(root.val);
+
+        VOT(root.left , x - 1 , y + 1 , map1);
+        VOT(root.right, x + 1 , y + 1 , map1);
+    }
+}
