14911ce2972da17c0ba887b1b78896e0ecbecc04
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-03 07:49:31 +0530

    Time: 1 ms (44.4%), Space: 44.9 MB (44.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,73 +1,49 @@
-class Pair{
-
-    int row;
-    int col;
-
-    Pair(int _row , int _col){
-        this.row = _row;
-        this.col = _col;
-    }
-
-}
-
-
-
-
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
 
-        Queue<Pair> queue1 = new LinkedList<>();
+        int nodeValue = image[sr][sc];
+        //preserve the previous value
+        int m = image.length;
+        int n = image[0].length;
 
-        queue1.offer(new Pair(sr , sc));
 
-        int n = image.length;
-        int m = image[0].length;
-
-        
-        int initialColour = image[sr][sc];
         image[sr][sc] = color;
-        int[][] visited = new int[n][m];
+        int[][] visited = new int[m][n];
         visited[sr][sc] = 1;
-        
-        
-        int[] drow = {-1 , 1 , 0 , 0};
-        int[] dcol = {0 , 0 , -1 , 1};
-
-        while(!queue1.isEmpty()){
-            
-            Pair temp = queue1.poll();
-            int tempRow = temp.row;
-            int tempCol = temp.col;
-
-            for(int i = 0 ; i < 4 ; i++){
-
-                int newRow = tempRow + drow[i];
-                int newCol = tempCol + dcol[i];
-
-                if((newRow >=0 && newRow < n) && (newCol >= 0 && newCol < m) && image[newRow][newCol] == initialColour){
-                    
-
-                    if(visited[newRow][newCol] == 1){
-                        continue;
-                    }
-
-                    System.out.println(newRow +" "+ newCol);
-                    image[newRow][newCol] = color;
-                    visited[newRow][newCol] = 1;
-                    queue1.offer(new Pair (newRow , newCol));
-
-                }
-
-            }
-
-
-        }
+        dfs(sr , sc , image , color , nodeValue , visited);
 
         return image;
 
 
 
         
+    }
+    public static void dfs( int row , int col , int[][] image , int color , int nodeValue , int[][] visited){
+
+
+        int[] drow = {-1 , 1 , 0 , 0};
+        int[] dcol = { 0 , 0 , -1 , 1};
+
+        for(int i = 0 ; i < 4 ; i++){
+            
+            int newRow = drow[i] + row;
+            int newCol = dcol[i] + col;
+            
+
+
+            if(newRow >= 0 && newRow < image.length && newCol >=0 && newCol < image[0].length && image[newRow][newCol] == nodeValue){
+                if(visited[newRow][newCol] == 1){
+                    continue;
+                }
+                image[newRow][newCol] = color;
+                visited[newRow][newCol] = 1;
+                dfs(newRow , newCol , image , color , nodeValue , visited);
+
+            }
+            
+        }
+
+
     }
 }
