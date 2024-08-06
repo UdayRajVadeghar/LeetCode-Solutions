46fe7e2867765608e64ac7a6df53f3230466a391
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-06 11:08:24 +0530

    Time: 8 ms (91.92%), Space: 45.6 MB (64.04%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,53 +1,33 @@
 class Solution {
     public int minimumPushes(String word) {
-
-        //8 total numbers adjustable
-
+        
         int[] arr = new int[26];
 
+        for(int i = 0 ; i < word.length() ; i++){
 
-        for(int i =  0 ; i < word.length() ; i++){
-            
             char temp = word.charAt(i);
 
             arr[temp - 'a']++;
-            
+
 
         }
-    
 
-        Queue<Integer> queue1 = new PriorityQueue(Collections.reverseOrder());
-
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] != 0){
-                //System.out.println(arr[i]);
-                queue1.offer(arr[i]);
-
-            }
-        }
-
-        int count = 0;
         int sum = 0;
 
-        while(!queue1.isEmpty()){
+        Arrays.sort(arr);
+        int count = 1;
+        int times = 0;
 
-            
-            count++;
-            for(int i = 0 ; i < 8 ; i++){
-
-                if(queue1.isEmpty()){
-                    break;
-                }
-                sum = sum + queue1.poll()*count;
-                System.out.println(sum);
+        for(int i = 25 ; i >= 0 ; i--){
+            if(times == 8){
+                count++;
+                times = 0;
             }
-            //System.out.println(sum);
-
+            sum = sum + count*arr[i];
+            times++;
         }
 
         return sum;
-
-        
         
     }
 }
