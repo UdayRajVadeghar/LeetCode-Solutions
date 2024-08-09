e523a2fa09bf5a41e188f89fb081182f771aba04
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-09 22:44:12 +0530

    Time: 0 ms (100%), Space: 40 MB (87.96%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.java
+++ 1086-divisor-game/1086-divisor-game.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean divisorGame(int n) {
 
         return n%2 ==0;
+        //Here we can understand the pattern and say that if it is even then alice would win it else bob could win it. This can be solved usingd dp aswell;
         
     }
 }
