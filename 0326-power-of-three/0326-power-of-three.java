a90e66c4f734da0e241f639591700d177ea713a7
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-10 23:11:29 +0530

    Time: 7 ms (100%), Space: 44.3 MB (10.74%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -2,17 +2,7 @@ class Solution {
     public boolean isPowerOfThree(int n) {
 
         
-
-        if(n == 1){
-            return true;
-        }
-
-        if(n == 0 || n%3 != 0){
-            return false;
-        }
-        
-
-        return isPowerOfThree(n/3);
+        return n > 0 && 1162261467%n == 0;
 
 
         
