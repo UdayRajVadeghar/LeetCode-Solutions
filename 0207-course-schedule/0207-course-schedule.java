881e2ac643e8f60cfd7ea3d56623617be83d77d7
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-10 21:42:46 +0530

    Time: 17 ms (9.24%), Space: 44.9 MB (50.24%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+      
+        int[] visited = new int[numCourses];
+        int[] pathVisited = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                if (dfs(i, prerequisites, visited, pathVisited)) {
+                    return false;  
+                }
+            }
+        }
+        return true;  
+    }
+
+    static boolean dfs(int node, int[][] prerequisites, int[] visited, int[] pathVisited) {
+        visited[node] = 1; 
+        pathVisited[node] = 1; 
+
+        for (int[] prereq : prerequisites) {
+            if (prereq[0] == node) {
+                int dependentCourse = prereq[1];
+                if (visited[dependentCourse] == 0) {
+                    if (dfs(dependentCourse, prerequisites, visited, pathVisited)) {
+                        return true;  
+                    }
+                } else if (pathVisited[dependentCourse] == 1) {
+                    return true;
+                }
+            }
+        }
+
+        pathVisited[node] = 0;  // Remove the node from the current path
+        return false;
+    }
+}
