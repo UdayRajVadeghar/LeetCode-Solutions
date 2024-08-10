87017aae604dabec49de4e36975dfddbaf55196d
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-08-10 22:53:48 +0530

    Time: 0 ms (100%), Space: 40.8 MB (39.27%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
 
-        if(n < 1){
-            return false;
-        }
+        return (n & n - 1) == 0 && (n - 1) % 3 == 0;
 
+        //since the power of 4 only occurs int the odd position in the bit representation , we can say that
+        // i) n&n-1 == 0 , 
+        // ii) (n-1)%3 == 0;
+        //by this we can say that it is a power of four
 
-        while(n > 1 ){
-            if(n%4 == 0){
-                n = n/4;
-            }else{
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
