2ae5e63b3f95a62ebcc9416648fb09072e43771e
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-10-07 11:52:51 +0530

    Time: 9 ms (17.26%), Space: 45 MB (12.38%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,13 +1,12 @@
 class Solution {
 
     public int minLength(String s) {
-        // Continue processing while "AB" or "CD" substrings exist
-        while (s.contains("AB") || s.contains("CD")) {
+     while (s.contains("AB") || s.contains("CD")) {
             if (s.contains("AB")) {
-                // Remove all occurrences of "AB"
+                
                 s = s.replace("AB", "");
             } else if (s.contains("CD")) {
-                // Remove all occurrences of "CD"
+                
                 s = s.replace("CD", "");
             }
         }
