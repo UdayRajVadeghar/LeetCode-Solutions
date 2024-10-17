686b69034fd5ca2db8736ad44a57774ed6093074
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-10-17 11:45:28 +0530

    Time: 0 ms (100%), Space: 44.7 MB (87.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -16,40 +16,61 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
 
-        //Using Iterative way
-        List<List<Integer>> list1 = new ArrayList<>();
-        if(root == null){
-            return list1;
-        }
+        // //Using Iterative way
+        // List<List<Integer>> list1 = new ArrayList<>();
+        // if(root == null){
+        //     return list1;
+        // }
 
-        Queue<TreeNode> queue1 = new LinkedList();
+        // Queue<TreeNode> queue1 = new LinkedList();
         
-        queue1.offer(root);
+        // queue1.offer(root);
 
         
 
-        while(!queue1.isEmpty()){
+        // while(!queue1.isEmpty()){
 
-            int size = queue1.size();
-            List<Integer> temp = new ArrayList<>();
+        //     int size = queue1.size();
+        //     List<Integer> temp = new ArrayList<>();
 
-            for(int i = 0 ; i < size ; i++){
+        //     for(int i = 0 ; i < size ; i++){
 
-                TreeNode x = queue1.poll();
+        //         TreeNode x = queue1.poll();
 
-                if(x.left != null){
-                    queue1.offer(x.left);
-                }
-                if(x.right != null){
-                    queue1.offer(x.right);
-                }
-                temp.add(x.val);
-            }
-            list1.add(new ArrayList<>(temp));
+        //         if(x.left != null){
+        //             queue1.offer(x.left);
+        //         }
+        //         if(x.right != null){
+        //             queue1.offer(x.right);
+        //         }
+        //         temp.add(x.val);
+        //     }
+        //     list1.add(new ArrayList<>(temp));
             
-        }
+        // }
 
+        // return list1;
+
+        //Recursive
+
+        List<List<Integer>> list1 = new ArrayList<>();
+        levelOrder(root , list1 , 1);
         return list1;
-        
+
+
+
+    }
+
+    public static void levelOrder(TreeNode root , List<List<Integer>> list1 , int level){
+
+        if(root == null){
+            return;
+        }
+        if(level > list1.size()){
+            list1.add(new ArrayList<>());
+        }
+        list1.get(level-1).add(root.val);
+        levelOrder(root.left , list1 , level+1);
+        levelOrder(root.right , list1 , level+1);
     }
 }
