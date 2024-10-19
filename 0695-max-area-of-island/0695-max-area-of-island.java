15e8caea6bde12da1174dac423ef936265b3c7c5
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-10-19 17:48:40 +0530

    Time: 2 ms (70.13%), Space: 44.4 MB (59.51%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,92 +1,48 @@
-class Pair{
-    int row;
-    int col;
-    int area;
-
-    Pair(int _row , int _col , int _area){
-        this.row = _row;
-        this.col = _col;
-        this.area = _area;
-    }
-}
-
-
-
-
-
 class Solution {
 
-  
+    static int[] xdir = {1 , -1 , 0 , 0};
+    static int[] ydir = {0 , 0 , 1 , -1};
 
     public int maxAreaOfIsland(int[][] grid) {
         
-
         int m = grid.length;
         int n = grid[0].length;
-        Queue<Pair> queue1 = new LinkedList<>();
         int[][] visited = new int[m][n];
+        int[] area = {0};
+        int maxArea = 0;
 
-        for(int i = 0 ; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == 1){
-                    queue1.offer(new Pair(i , j , 1));
-                    
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+
+                if(visited[i][j] == 0 && grid[i][j] == 1){
+                    dfs(grid , visited , area , i , j , m , n);
+                    maxArea = Math.max(maxArea , area[0]);
+                    area[0] = 0;
                 }
+
             }
         }
 
-        int maxArea = 0;
-        while(!queue1.isEmpty()){
-            
-            Pair temp = queue1.poll();
-            int row = temp.row;
-            int col = temp.col;
-            int area = temp.area;
-
-            
-            visited[row][col] = 1;
-            //System.out.println("   " + row +" "+col);
-            int[] currentArea = {0};
-            dfs(row , col , area , grid , visited , currentArea);
-            maxArea = Math.max(currentArea[0] , maxArea);
-
-        }
-
-
         return maxArea;
-
-    
-
         
     }
-    public static void dfs(int row , int col , int area , int[][] grid , int[][]visited , int[] currentArea){
+    public static void dfs(int[][] grid , int[][] visited , int[] area , int row , int col , int m , int n){
 
-        int[] drow = {-1 , 1 , 0 , 0 };
-        int[] dcol = {0 , 0 , -1 , 1};
-        System.out.println(area);
-        currentArea[0]++;
+        visited[row][col] = 1;
+        area[0]++;
 
         for(int i = 0 ; i < 4 ; i++){
 
-            int newRow = drow[i] + row;
-            int newCol = dcol[i] + col;
+            int newRow = row + xdir[i];
+            int newCol = col + ydir[i];
 
-            if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == 1){
+            if(newRow >=0 && newRow < m && newCol >= 0 && newCol < n && visited[newRow][newCol] != 1 && grid[newRow][newCol] == 1){
 
-                if(visited[newRow][newCol] == 1){
-                    continue;
-                }
-
-                visited[newRow][newCol] = 1;
-                //System.out.println(newRow + " " + newCol);
-                
-                dfs(newRow , newCol , area , grid , visited , currentArea);
+                dfs(grid , visited , area , newRow , newCol , m , n);
 
             }
 
         }
-
-
 
     }
 }
