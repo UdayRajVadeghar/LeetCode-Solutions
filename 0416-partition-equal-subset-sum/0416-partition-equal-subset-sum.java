5855fc7442f5b96abd1a87239d8127231727e1d0
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-10-25 23:02:42 +0530

    Time: 100 ms (18.06%), Space: 57 MB (32.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,45 +5,47 @@ class Solution {
 
         for(int i = 0 ; i < nums.length ; i++){
 
-            sum = nums[i] + sum;
-            
+            sum = sum + nums[i];
         }
 
         if(sum%2 == 1){
             return false;
         }
 
-        Boolean[][] dp = new Boolean[nums.length][sum];
+        int target = sum/2;
 
-        return recurse(nums , 0 , sum , 0 , dp);
+        Boolean[][] dp = new Boolean[nums.length+1][target+1];
 
-        
-        
+        return recurse(nums , target , 0 , 0 , dp);
         
     }
-    public static boolean recurse(int[] nums , int index , int sum , int curSum , Boolean[][] dp){
+    public static boolean recurse(int[] nums , int target , int index , int curSum , Boolean[][] dp){
+
 
         if(index >= nums.length){
-            if(curSum == sum){
+
+            if(curSum == target){
                 return true;
             }else{
                 return false;
             }
+
         }
-        if(dp[index][curSum] != null){
-            return dp[index][curSum];
-        }
-        if(curSum > sum){
+
+        if(curSum > target){
             return false;
         }
 
-        boolean pick = recurse(nums , index + 1 , sum - nums[index] , curSum + nums[index] , dp);
-        boolean notPick = recurse(nums, index + 1, sum , curSum , dp);
+        if(dp[index][curSum] != null){
+            return dp[index][curSum];
+        }
+
+        boolean pick = recurse(nums , target , index + 1 ,curSum + nums[index] , dp);
+        boolean notPick = recurse(nums , target , index+1 , curSum , dp);
 
         dp[index][curSum] = pick || notPick;
 
         return dp[index][curSum];
-        
 
 
     }
