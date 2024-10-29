c72ff34332e629808d3a5a6283b54f4195024b54
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-10-29 23:49:51 +0530

    Time: 26 ms (35.13%), Space: 44.8 MB (87.56%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,40 +1,49 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int maximum = max(piles);
-        System.out.println(maximum);
-        
+
         int low = 1;
-        int high = maximum;
+        int high = 0;
 
-        while (low <= high) {
+        for(int i = 0 ; i < piles.length ; i++){
+
+            high = Math.max(high , piles[i]);
+
+        }
+
+
+
+
+        while(low <= high){
+
             int mid = low + (high-low) / 2;
-            int temp = hours(piles, mid);
+            int expected = check(piles , mid);
 
-            if (temp <= h) {
+            if(expected > h){
+                low = mid+1;
+            }else{
                 high = mid - 1;
-            } else {
-                low = mid + 1;
             }
-        }
+
+
+        }        
+
         return low;
+        
     }
+    public static int check(int[] piles , int val){
 
-    public static int max(int[] piles) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < piles.length; i++) {
-            max = Math.max(max, piles[i]);
-        }
-        return max;
-    }
-
-    public static int hours(int[] piles, int num) {
         int hours = 0;
-        for (int i = 0; i < piles.length; i++) {
+       
 
-            hours += Math.ceil((double)piles[i]/(double)num);
+        for(int i = 0 ; i < piles.length ; i++){
+
+            hours += Math.ceil((double)piles[i]/(double)val);
+
 
         }
-        System.out.println(hours);
+        
+
         return hours;
+
     }
 }
