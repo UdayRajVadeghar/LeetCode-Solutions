05e35da421d22d02cd7e98870e120791da71b5f8
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-01 13:31:55 +0530

    Time: 30 ms (16.99%), Space: 62 MB (5.1%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -36,29 +36,31 @@ class Solution {
     }
     public static int lcs(String text1 , String text2 , int n , int m , Integer[][] dp){
 
-        if(n==0 || m ==0){
+        if(m == 0 || n == 0){
             return 0;
         }
 
-        if(dp[n][m] != null){
-            return dp[n][m];
+        if(dp[n-1][m-1] != null){
+            return dp[n-1][m-1];
         }
 
-        
+       
 
         if(text1.charAt(n-1) == text2.charAt(m-1)){
-
-            int common = 1 + lcs(text1,text2,n-1,m-1,dp);
-            return dp[n][m] = common;
+            int common =  1+lcs(text1,text2,n-1,m-1,dp);
+            dp[n-1][m-1] = common;
+            return dp[n-1][m-1];
         }else{
-            int uncommon = Math.max(lcs(text1,text2,n-1,m,dp) , lcs(text1,text2,n,m-1,dp));
-            return dp[n][m] = uncommon;
+            int uncommon = Math.max(lcs(text1 , text2 , n , m-1 ,dp ) , lcs(text1 , text2 , n-1 , m , dp));
+            dp[n-1][m-1] = uncommon;
+            return dp[n-1][m-1];
         }
 
         
 
-        
-
+      
+ 
 
     }
+    
 }
