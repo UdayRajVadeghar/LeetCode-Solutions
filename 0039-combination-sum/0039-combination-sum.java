7bb1afef8fc7538316f89879808812b2ab172907
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-02 15:47:13 +0530

    Time: 2 ms (84.87%), Space: 44.5 MB (50.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,35 +1,36 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        List<List<Integer>> list1 = new ArrayList<>();
+
         List<Integer> list2 = new ArrayList<>();
+        List<List<Integer>> list1 = new ArrayList<>();
 
         recurse(candidates , target , 0 , list1 , list2);
 
         return list1;
 
+
+        
     }
-    public static void recurse(int[] candidates , int target , int index ,List<List<Integer>> list1 , List<Integer> list2){
+    public static void recurse(int[] candidates , int target , int index , List<List<Integer>> list1 , List<Integer> list2){
 
-        if(index >= candidates.length){
-
-            if(target == 0){
-            
-                list1.add(new ArrayList<>(list2));
-                
-            }
+        if(target < 0){
             return;
         }
 
-        if(target - candidates[index] >= 0){
-            list2.add(candidates[index]);
-            recurse(candidates , target - candidates[index] , index , list1 , list2);
-            list2.removeLast();
-        }
-       
-        
+        if(index >= candidates.length){
+            
+            if(target == 0){
+                list1.add(new ArrayList<>(list2));
+            }
+            return;
 
-        recurse(candidates,target,index+1 , list1 , list2);
+            
+        }
+
+        list2.add(candidates[index]);
+        recurse(candidates , target - candidates[index] , index , list1 , list2);
+        list2.removeLast();
+        recurse(candidates, target , index+1 , list1 , list2);
 
     }
 }
