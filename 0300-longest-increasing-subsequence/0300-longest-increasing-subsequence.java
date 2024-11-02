1f6cad1da5099a7b12aeb46ed6fd91df6d203e4a
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-02 16:40:11 +0530

    Time: 435 ms (5.04%), Space: 195 MB (5.43%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        Integer[][] dp = new Integer[nums.length+1][nums.length+1];
+        return recurse(nums,0,-1,dp);
+        
+    }
+    public static int recurse(int[] nums , int index , int lastPickedIndex , Integer[][] dp){
+
+        if(index >= nums.length){
+            return 0;
+        }
+
+        if(dp[index][lastPickedIndex+1] != null){
+
+            return dp[index][lastPickedIndex+1];
+        }
+
+        int pick = 0;
+
+        if(lastPickedIndex == -1 || nums[lastPickedIndex] < nums[index]){
+
+            pick = 1+recurse(nums,index+1,index,dp);
+
+        }
+
+        int notPick = recurse(nums,index+1,lastPickedIndex,dp);
+
+        dp[index][lastPickedIndex+1] = Math.max(pick , notPick);
+
+        return dp[index][lastPickedIndex+1];
+
+    }
+}
