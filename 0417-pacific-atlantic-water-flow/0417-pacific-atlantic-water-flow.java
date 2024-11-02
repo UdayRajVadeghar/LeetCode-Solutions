2bce34381d5d504c753f8950c950ea9b0a52a84a
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-02 21:26:33 +0530

    Time: 6 ms (47.8%), Space: 45.5 MB (53.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,55 +1,128 @@
 class Solution {
-    boolean b;
-    char flow(int i,int m,int j,int n){
-        if((i==0&&j==n-1)||(i==m-1&&j==0)||b){
-            return 'b';
-        }
-        if(dp[i][j]!='\u0000') return dp[i][j];
-        char u=i==0?'p':'n',l=j==0?'p':'n',r=j==n-1?'a':'n',d=i==m-1?'a':'n';
-        int t=heights[i][j];
-        heights[i][j]=-1;
-        //up
-        if(u=='n'&&t>=heights[i-1][j]&&heights[i-1][j]!=-1){
-            u=flow(i-1,m,j,n);
-        }
-        //down
-        if(u!='b'&&d=='n'&&t>=heights[i+1][j]&&heights[i+1][j]!=-1){
-            d=flow(i+1,m,j,n);
-        }        
-        //right
-        if(u!='b'&&d!='b'&&r=='n'&&t>=heights[i][j+1]&&heights[i][j+1]!=-1){
-            r=flow(i,m,j+1,n);
-        }        
-        //left
-        if(r!='b'&&u!='b'&&d!='b'&&l=='n'&&t>=heights[i][j-1]&&heights[i][j-1]!=-1){
-            l=flow(i,m,j-1,n);
-        }    
-        heights[i][j]=t;
-        char ans='n';
-        if(r=='b'||l=='b'||u=='b'||d=='b') ans = 'b';
-        else if((r=='a'||l=='a'||u=='a'||d=='a')&&(r=='p'||l=='p'||u=='p'||d=='p')) ans = 'b';
-        else ans = (r=='a'||l=='a'||u=='a'||d=='a')?'a':(r=='p'||l=='p'||u=='p'||d=='p')?'p':'n';
-        if(ans=='b') b=true;
-        return ans;
-    }
-    int[][] heights;List<List<Integer>> ans = new ArrayList<>();
-    char[][] dp;
+
+    //4 directions
+    static int[] drow = {0,0,1,-1};
+    static int[] dcol = {1,-1,0,0};
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        this.heights=heights;
-        int m=heights.length,n=heights[0].length;
-        dp=new char[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                b=false;
-                if(dp[i][j]=='\u0000')
-                    dp[i][j]=flow(i,m,j,n);
-                if(dp[i][j]=='b'){
-                    ArrayList<Integer> a = new ArrayList<>();
-                    a.add(i);a.add(j);
-                    ans.add(a);
+
+        int m = heights.length;
+        int n = heights[0].length;
+
+        int[][] visited = new int[m][n];
+
+        //pacific ocean
+
+        //1st row
+
+        for(int i = 0 ; i < n ; i++){
+            
+            if(visited[0][i] == 0){
+                
+                dfsP(heights,0,i,visited,m,n);
+            }
+
+        }
+
+        //1st col
+
+        for(int i = 0 ; i < m ; i++){
+            
+            if(visited[i][0] == 0){
+                dfsP(heights,i,0,visited,m,n);
+            }
+
+        }
+
+
+        
+        
+        //Atlantic Ocean
+
+        //last row
+
+        for(int i = 0 ; i < n ; i++){
+            
+            if(visited[m-1][i] == 0 || visited[m-1][i] == 1){
+                dfsA(heights,m-1,i,visited,m,n);
+            }
+
+        }
+
+        //last col
+
+        for(int i = 0 ; i < m ; i++){
+            
+            if(visited[i][n-1] == 0 || visited[i][n-1] == 1){
+                dfsA(heights,i,n-1,visited,m,n);
+            }
+
+        }
+        
+
+        List<List<Integer>> list1 = new ArrayList<>();
+
+        for(int i = 0 ; i < m ; i++){
+
+            for(int j = 0 ; j < n ; j++){
+                List<Integer> list2 = new ArrayList<>();
+
+                if(visited[i][j] == 3){
+                    list2.add(i);
+                    list2.add(j);
+                    list1.add(new ArrayList<>(list2));
+                }
+
+            }
+
+        }
+
+        return list1;
+
+
+        
+    }
+    public static void dfsP(int[][] heights , int row , int col , int[][] visited ,int m , int n){
+
+        visited[row][col] = 1;
+
+        for(int i = 0 ; i < 4 ; i++){
+
+            int newRow = row + drow[i];
+            int newCol = col + dcol[i];
+
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
+
+                if(heights[newRow][newCol] >= heights[row][col] && visited[newRow][newCol] != 1){
+
+                    dfsP(heights,newRow,newCol,visited,m,n);
+
                 }
             }
         }
-        return ans;
     }
+
+    public static void dfsA(int[][] heights , int row , int col , int[][] visited ,int m , int n){
+
+        visited[row][col] += 2;
+
+        for(int i = 0 ; i < 4 ; i++){
+
+            int newRow = row + drow[i];
+            int newCol = col + dcol[i];
+
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
+
+                if(heights[newRow][newCol] >= heights[row][col] && visited[newRow][newCol] != 2 && visited[newRow][newCol] != 3){
+
+                    dfsA(heights,newRow,newCol,visited,m,n);
+
+                }
+            }
+
+        }
+
+    }
+
+
 }
