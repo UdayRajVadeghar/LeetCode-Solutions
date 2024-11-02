e4373cf59dcd071a95ed7c6071084488a7283e45
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-02 16:42:19 +0530

    Time: 466 ms (5.04%), Space: 194.5 MB (5.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,4 +1,37 @@
 class Solution {
+
+    //Recursion TLE
+    // public int lengthOfLIS(int[] nums) {
+        
+    //     Integer[][] dp = new Integer[nums.length+1][nums.length+1];
+    //     return recurse(nums,0,0,Integer.MIN_VALUE,-1,dp);
+        
+    // }
+    // public static int recurse(int[] nums , int index , int length , int lastPicked , int lastPickedIndex , Integer[][] dp){
+
+    //     if(index == nums.length){
+    //         return length;
+    //     }
+        
+    //     if(dp[index][lastPickedIndex+1] != null){
+    //         return dp[index][lastPickedIndex+1];
+    //     }
+    //     int left = 0;
+
+    //     if (lastPickedIndex == -1 || lastPicked < nums[index]) {
+    //         left = recurse(nums, index + 1, length + 1, nums[index], index, dp);
+    //     }   
+
+    //     int right = recurse(nums,index+1,length,lastPicked,lastPickedIndex,dp);
+        
+
+        
+    //     dp[index][lastPickedIndex+1] = Math.max(left , right);
+
+    //     return dp[index][lastPickedIndex+1];
+
+    // }
+
     public int lengthOfLIS(int[] nums) {
         
         Integer[][] dp = new Integer[nums.length+1][nums.length+1];
