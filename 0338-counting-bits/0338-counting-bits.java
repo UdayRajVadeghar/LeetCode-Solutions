750611340e7c5ab13f76bcc25bebb67a21b64adc
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-03 13:37:52 +0530

    Time: 2 ms (96.98%), Space: 46.5 MB (68.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -5,24 +5,12 @@ class Solution {
 
         for(int i = 0 ; i < arr.length ; i++){
 
-            arr[i] = ones(i);
+            arr[i] = Integer.bitCount(i);
 
         }
 
         return arr;
         
     }
-    public static int ones(int n){
-
-        int count = 0;
-
-        while(n > 0){
-
-            n = n&(n-1);
-            count++;
-
-        }
-        return count;
-
-    }
+    
 }
