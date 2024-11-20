f297e76056274a5c4f31fb82f3e868f995259168
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-11-20 10:23:40 +0530

    Time: 463 ms (5%), Space: 194.8 MB (5.29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -32,36 +32,81 @@ class Solution {
 
     // }
 
-    public int lengthOfLIS(int[] nums) {
-        
-        Integer[][] dp = new Integer[nums.length+1][nums.length+1];
-        return recurse(nums,0,-1,dp);
-        
-    }
-    public static int recurse(int[] nums , int index , int lastPickedIndex , Integer[][] dp){
+    //-----------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
 
-        if(index >= nums.length){
+    //404 MS Memoization - Learn Tabulation Soon
+    
+    // public int lengthOfLIS(int[] nums) {
+        
+    //     Integer[][] dp = new Integer[nums.length+1][nums.length+1];
+    //     return recurse(nums,0,-1,dp);
+        
+    // }
+    // public static int recurse(int[] nums , int index , int lastPickedIndex , Integer[][] dp){
+
+    //     if(index >= nums.length){
+    //         return 0;
+    //     }
+
+    //     if(dp[index][lastPickedIndex+1] != null){
+
+    //         return dp[index][lastPickedIndex+1];
+    //     }
+
+    //     int pick = 0;
+
+    //     if(lastPickedIndex == -1 || nums[lastPickedIndex] < nums[index]){
+
+    //         pick = 1+recurse(nums,index+1,index,dp);
+
+    //     }
+
+    //     int notPick = recurse(nums,index+1,lastPickedIndex,dp);
+
+    //     dp[index][lastPickedIndex+1] = Math.max(pick , notPick);
+
+    //     return dp[index][lastPickedIndex+1];
+
+    // }
+
+
+
+    public int lengthOfLIS(int[] nums){
+
+        Integer[][] dp = new Integer[nums.length+1][nums.length+2];
+
+        return recurse(nums , 0 , nums.length , -1 , dp);
+
+
+
+    }
+    public static int recurse(int[] nums , int index , int length ,int lastPicked , Integer[][] dp){
+
+        if(index == length){
             return 0;
         }
 
-        if(dp[index][lastPickedIndex+1] != null){
-
-            return dp[index][lastPickedIndex+1];
+        if(dp[index][lastPicked+1] != null){
+            return dp[index][lastPicked+1];
         }
 
-        int pick = 0;
+        //pick not pick
 
-        if(lastPickedIndex == -1 || nums[lastPickedIndex] < nums[index]){
+        int left = 0;
+        
 
-            pick = 1+recurse(nums,index+1,index,dp);
-
+        if(lastPicked == -1 || nums[index] > nums[lastPicked]){
+            left = 1 + recurse(nums , index + 1, length , index , dp);
         }
+            
+        int right = recurse(nums , index + 1 , length , lastPicked , dp);
+        
 
-        int notPick = recurse(nums,index+1,lastPickedIndex,dp);
+        dp[index][lastPicked+1] = Math.max(left , right);
 
-        dp[index][lastPickedIndex+1] = Math.max(pick , notPick);
+        return dp[index][lastPicked+1];
 
-        return dp[index][lastPickedIndex+1];
 
     }
 }
