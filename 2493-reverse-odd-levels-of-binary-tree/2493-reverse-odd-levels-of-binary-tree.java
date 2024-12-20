9aa60d2a1d8484059ce17cd64265eb4f193c39a0
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2024-12-20 20:35:03 +0530

    Time: 0 ms (100%), Space: 48 MB (82.66%) - LeetHub

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        
+        traverse(root.left, root.right, 1);
+        return root;
+    }
+
+    public void traverse(TreeNode node1, TreeNode node2, int lvl) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        if (lvl % 2 == 1){
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
+        }
+
+        traverse(node1.left, node2.right, lvl + 1);
+        traverse(node1.right, node2.left, lvl + 1);
+    }
+}
+
