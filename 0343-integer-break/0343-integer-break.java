32699c69eafd2b730c0ef3742c7f327de8358a49
Uday Raj <98938345+UdayRajVadeghar@users.noreply.github.com>
2025-01-12 01:06:51 +0530

    Time: 0 ms (100%), Space: 40.7 MB (21.63%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,20 +2,26 @@ class Solution {
     public int integerBreak(int n) {
         if(n==2) return 1;
         if(n==3) return 2;
-        return recurse(n , 1);
+        Integer dp[] = new Integer[n + 1];
+        return recurse(n , 1 , dp);
     }
-    public static int recurse(int n , int product ){
+    public static int recurse(int n , int product , Integer[] dp){
         
-        if(n < -1){
+        if(n < 0){
             return 0;
         }
 
         if(n == 0){
             return product;
         }
+
+        if(dp[n] != null){
+            return dp[n];
+        }
         
-        int max = Math.max(recurse(n-3 , product*3) , recurse(n-2 , product * 2));
-    
+        int max = Math.max(recurse(n-3 , product*3 , dp) , recurse(n-2 , product * 2 , dp));
+
+        dp[n] = max;
 
         return max;
     }
